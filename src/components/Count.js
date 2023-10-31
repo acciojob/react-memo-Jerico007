a5fb38e532dd0887263e6191604887f7f0c86fb6
Jerico <jericodcosta95375788@gmail.com>
2023-10-31 12:14:34 +0530

    fc

--- src/components/Count.js
+++ src/components/Count.js
@@ -6,8 +6,8 @@ const Count = () => {
     return (
         <div className='Count'>
             <div>
-                <p style={{display:"inline"}}>Count :{count}</p>
-                <button id='incr-cnt' onClick={()=>{setCount(count+1)}}>{count}</button>
+                <p id='incr-cnt' style={{display:"inline"}}>Count :{count}</p>
+                <button id='incr-btn' onClick={()=>{setCount(count+1)}}>{count}</button>
             </div>
         </div>
     );
